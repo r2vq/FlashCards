f6567cbfef5ea0847ed5b2c119b192a4ca538675
Keane Quibilan <keane.quibilan@loblaw.ca>
2021-03-21 03:11:24 -0400

    Stay on Add Card screen when submitting new card

--- screenadd/src/main/java/com/keanequibilan/screenadd/AddFragment.kt
+++ screenadd/src/main/java/com/keanequibilan/screenadd/AddFragment.kt
@@ -1,14 +1,11 @@
 package com.keanequibilan.screenadd
 
-import android.content.Context
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.view.inputmethod.InputMethodManager
 import androidx.appcompat.widget.AppCompatButton
 import androidx.fragment.app.Fragment
-import androidx.navigation.fragment.NavHostFragment.findNavController
 import com.google.android.material.textfield.TextInputLayout
 import com.keanequibilan.screenadd.util.getTextOrNull
 import com.keanequibilan.screenadd.viewmodel.AddViewModel
@@ -23,7 +20,6 @@ class AddFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         val view = inflater.inflate(R.layout.fragment_add, container, false)
-        val imm = context?.getSystemService(Context.INPUT_METHOD_SERVICE) as? InputMethodManager
 
         val tilFront = view.findViewById<TextInputLayout>(R.id.el_front)
         val tilBack = view.findViewById<TextInputLayout>(R.id.el_back)
@@ -35,8 +31,8 @@ class AddFragment : Fragment() {
             if (front != null && back != null) {
                 addViewModel.createCard(front, back)
 
-                findNavController(this).popBackStack()
-                imm?.hideSoftInputFromWindow(view.windowToken, 0)
+                tilFront.editText?.text?.clear()
+                tilBack.editText?.text?.clear()
             }
         }
         return view
