379c129d790c4ca1025526e7c5017292ba0e9840
Keane Quibilan <keane.quibilan@loblaw.ca>
2021-03-24 23:14:19 -0400

    Stylize details page

--- screenlist/src/main/java/com/keanequibilan/screenlist/viewholder/DetailViewHolder.kt
+++ screenlist/src/main/java/com/keanequibilan/screenlist/viewholder/DetailViewHolder.kt
@@ -3,6 +3,7 @@ package com.keanequibilan.screenlist.viewholder
 import android.view.View
 import androidx.appcompat.widget.AppCompatTextView
 import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.chip.Chip
 import com.keanequibilan.screenlist.R
 import com.keanequibilan.screenlist.model.DetailCardItem
 import com.keanequibilan.screenlist.model.DetailItem
@@ -29,13 +30,13 @@ internal class DetailCardViewHolder(itemView: View) : DetailViewHolder(itemView)
 
 internal class DetailSessionViewHolder(itemView: View) : DetailViewHolder(itemView) {
     private val tvId: AppCompatTextView = itemView.findViewById(R.id.tv_id)
-    private val tvCorrect: AppCompatTextView = itemView.findViewById(R.id.tv_correct)
-    private val tvIncorrect: AppCompatTextView = itemView.findViewById(R.id.tv_incorrect)
-    private val tvSkip: AppCompatTextView = itemView.findViewById(R.id.tv_skip)
+    private val tvCorrect: Chip = itemView.findViewById(R.id.tv_correct)
+    private val tvIncorrect: Chip = itemView.findViewById(R.id.tv_incorrect)
+    private val tvSkip: Chip = itemView.findViewById(R.id.tv_skip)
 
     override fun bind(item: DetailItem) {
         (item as? DetailSessionItem)?.let {
-            tvId.text = "Session: ${it.id}"
+            tvId.text = "Session ${it.id}:"
             tvCorrect.text = "Correct: ${it.correct}"
             tvIncorrect.text = "Incorrect: ${it.incorrect}"
             tvSkip.text = "Skip: ${it.skips}"
