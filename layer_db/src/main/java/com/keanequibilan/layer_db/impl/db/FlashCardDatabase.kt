9b0bb5307feba9982f96af58a1af5b34be0806e2
Keane Quibilan <keane.quibilan@loblaw.ca>
2021-03-26 21:33:41 -0400

    Add PlantUML docs for Database

--- layer_db/src/main/java/com/keanequibilan/layer_db/impl/db/FlashCardDatabase.kt
+++ layer_db/src/main/java/com/keanequibilan/layer_db/impl/db/FlashCardDatabase.kt
@@ -22,6 +22,50 @@ import com.keanequibilan.layer_db.impl.entity.SessionEntity
     ],
     version = 1
 )
+/**
+ * @startuml
+ * hide circle
+ * skinparam linetype ortho
+ *
+ * entity "FlashCardTable" as card {
+ *  *id : int <<generated>>
+ * --
+ *  *front : text
+ *  *back : text
+ * }
+ *
+ * entity "SessionTable" as session {
+ *  *id : number <<generated>>
+ * }
+ *
+ * entity "CardSessionTable" as cardSession {
+ *  *cardId : int <<FK>>
+ *  *sessionId : int <<FK>>
+ * --
+ *  *correct : int
+ *  *incorrect : int
+ *  *skip : int
+ * }
+ *
+ * entity "CardSessionView" as cardSessionView {
+ *  *cardId : int <<FK>>
+ *  *sessionId : int <<FK>>
+ * --
+ *  *front : text
+ *  *back : text
+ *  *correct : int
+ *  *incorrect : int
+ *  *skip : int
+ * }
+ *
+ * note bottom: This shows only the CardSessions\nwith the most recent session. If any\nFlashCards aren't in the most recent\nCardSessionTable, it will still show\nthe FlashCard values but it will\ndefault "correct", "incorrect",and\n"skip" to 0.
+ *
+ * card ||..o{ cardSession
+ * cardSession }o..|| session
+ * cardSessionView |o--|| cardSession
+ * cardSessionView |o--|| card
+ * @enduml
+ */
 internal abstract class FlashCardDatabase : RoomDatabase() {
     abstract fun flashCardDao(): FlashCardDao
     abstract fun sessionDao(): SessionDao
